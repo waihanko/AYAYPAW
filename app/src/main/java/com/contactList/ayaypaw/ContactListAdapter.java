91af6b71d0593ea0bb7cc3b7b07d2bc1ec3d97a4
PhyoeKo <bagyiphyoeko1996@gmail.com>
2020-04-02 22:50:30 +0630

    added recycler view

---
+++ app/src/main/java/com/contactList/ayaypaw/ContactListAdapter.java
@@ -0,0 +1,62 @@
+package com.contactList.ayaypaw;
+
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.contactList.ayaypaw.data_model.ContactModel;
+
+import java.util.List;
+
+
+public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.MyViewHolder> {
+
+    private List<ContactModel> contactModelList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView stateName, personName, personRole,phoneNumber,townshipName;
+
+        public MyViewHolder(View view) {
+            super(view);
+            stateName = (TextView) view.findViewById(R.id.stateName);
+            personName = (TextView) view.findViewById(R.id.personName);
+            personRole = (TextView) view.findViewById(R.id.personRole);
+            phoneNumber = (TextView) view.findViewById(R.id.phoneNumber);
+            townshipName = (TextView) view.findViewById(R.id.townshipName);
+
+        }
+    }
+
+
+    public ContactListAdapter(List<ContactModel> moviesList) {
+        this.contactModelList = moviesList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.contact_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        ContactModel contactModel = contactModelList.get(position);
+        holder.stateName.setText(contactModel.getStateName());
+        holder.personName.setText(contactModel.getPersonName());
+        holder.personRole.setText(contactModel.getPersonRole());
+        holder.phoneNumber.setText(contactModel.getPhoneNumber());
+        holder.townshipName.setText(contactModel.getTownshipName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactModelList.size();
+    }
+}
--- app/src/main/java/com/contactList/ayaypaw/MainActivity.java
+++ app/src/main/java/com/contactList/ayaypaw/MainActivity.java
@@ -1,14 +1,55 @@
 package com.contactList.ayaypaw;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.contactList.ayaypaw.data_model.ContactModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<ContactModel> contactList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private ContactListAdapter contactListAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.contact_list);
+
+        contactListAdapter = new ContactListAdapter(contactList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(contactListAdapter);
+
+        prepareMovieData();
+    }
+
+    private void prepareMovieData() {
+        ContactModel contactModel = new ContactModel("Yangon", "General Manager", "Dagon","0978256478","PhyoeKo");
+        contactList.add(contactModel);
+
+
+         contactModel = new ContactModel("Yangon", "General Manager", "Dagon","0978256478","PhyoeKo");
+        contactList.add(contactModel);
+
+
+        contactModel = new ContactModel("Yangon", "General Manager", "Dagon","0978256478","PhyoeKo");
+        contactList.add(contactModel);
+
+         contactModel = new ContactModel("Yangon", "General Manager", "Dagon","0978256478","PhyoeKo");
+        contactList.add(contactModel);
+
+
+
+
+        contactListAdapter.notifyDataSetChanged();
     }
 }
