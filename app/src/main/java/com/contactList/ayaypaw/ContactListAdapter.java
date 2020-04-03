8c19001947ee76fac5c5dbbea4e69454580ee88f
PhyoeKo <bagyiphyoeko1996@gmail.com>
2020-04-03 17:45:04 +0630

    search function added

--- app/src/main/java/com/contactList/ayaypaw/ContactListAdapter.java
+++ app/src/main/java/com/contactList/ayaypaw/ContactListAdapter.java
@@ -6,9 +6,11 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,12 +19,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.contactList.ayaypaw.data_model.ContactModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.MyViewHolder> {
 
     private List<ContactModel> contactModelList;
+    private List<ContactModel> contactListFiltered;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         private TextView stateName, personName, personRole, phoneNumber, departmentName,tv_responsibility;
@@ -120,4 +124,41 @@ public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.
     public int getItemCount() {
         return contactModelList.size();
     }
+
+
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    contactListFiltered = contactModelList;
+                } else {
+                    List<ContactModel> filteredList = new ArrayList<>();
+                    for (ContactModel row : contactModelList) {
+
+                        // name match condition. this might differ depending on your requirement
+                        // here we are looking for name or phone number match
+                        if (row.getStateCode().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row);
+                        }
+                    }
+
+                    contactListFiltered = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = contactListFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                contactListFiltered = (ArrayList<ContactModel>) filterResults.values;
+                Log.i("SEARCH",contactListFiltered.size()+"");
+                notifyDataSetChanged();
+            }
+        };
+    }
+
 }
--- app/src/main/java/com/contactList/ayaypaw/MainActivity.java
+++ app/src/main/java/com/contactList/ayaypaw/MainActivity.java
@@ -6,14 +6,18 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.contactList.ayaypaw.data.StateCreator;
 import com.contactList.ayaypaw.model.EmergencyDataModel;
 import com.contactList.ayaypaw.retrofit.API_Interface;
 import com.contactList.ayaypaw.retrofit.ApiUtils;
@@ -39,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     List<List<String>> value;
     private String TAG = "Response";
     private String contactData[][];
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,19 +110,50 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-
+        Log.i("GET_STATE", StateCreator.getInstance().getAllStateList().size()+"");
         getMenuInflater().inflate(R.menu.option_menu, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.action_search)
+                .getActionView();
+        searchView.setSearchableInfo(searchManager
+                .getSearchableInfo(getComponentName()));
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        // listening to search query text change
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // filter recycler view when query submitted
+                contactListAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // filter recycler view when text is changed
+                contactListAdapter.getFilter().filter(query);
+                recyclerView.setAdapter(contactListAdapter);
+
+                return false;
+            }
+        });
+
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.it_Filter : break;
-            case R.id.it_About : {
-                startActivity(new Intent(this, AboutActivity.class));
-            }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
